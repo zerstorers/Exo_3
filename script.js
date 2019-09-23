b641d40036b8452a6b81e444725c5cab360cf54e
zerstorers <lemaire-maxime@hotmail.fr>
2019-09-23 11:24:47 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// var l = [1, 13, 15, 76, 45, 14, 34, 21, 34];
+// var i = 0;
+
+// //AVEC UNE BOUCLE WHILE
+// while(i < l.length) {
+//     if(l[i] > 10 && l[i] < 20){
+//         console.log(l[i], "ok");
+//     } else {
+//         console.log(l[i], "pas ok");
+//     }
+//     i++;
+// }
+
+// //AVEC UNE BOUCLE FOR
+// for (let i = 0; i < l.length; i++) {
+//     if(l[i] > 10 && l[i] < 20){
+//         console.log(l[i], "ok");
+//     } else {
+//         console.log(l[i], "pas ok");
+//     }
+// }
